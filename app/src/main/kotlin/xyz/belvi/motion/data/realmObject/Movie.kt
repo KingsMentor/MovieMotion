ceab062313949c9452c520245771fbc2a2e0e7d2
Nosakhare Belvi <nosakharebelvi@gmail.com>
2018-06-24 20:53:46 +0100

    kotlin structure

--- app/src/main/kotlin/xyz/belvi/motion/data/realmObject/Movie.kt
+++ app/src/main/kotlin/xyz/belvi/motion/data/realmObject/Movie.kt
@@ -5,6 +5,7 @@ import xyz.belvi.motion.models.enums.MoviePosterSize
 /**
  * Created by zone2 on 6/23/18.
  */
+
 interface Movie {
     fun getMovieTitle(): String
     fun getMoviePosterPath(moviePosterSize: MoviePosterSize): String
